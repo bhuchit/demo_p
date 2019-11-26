643e66220f979b9746ba05e6d8f16f255f5831da
bhu <poochit1996@hotmail.com>
2019-11-26 13:54:46 +0700

    update rout

--- hello_world/src/app/app-routing.module.ts
+++ hello_world/src/app/app-routing.module.ts
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'history', loadChildren: './history/history.module#HistoryPageModule' },
-  { path: 'bill-detail', loadChildren: './bill-detail/bill-detail.module#BillDetailPageModule' },
->>>>>>> 68bda24396e7d1a05715c93fe2b3cc28b0305ec8
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
